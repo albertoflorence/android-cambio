04f7385d3a23d39ee79b7aa4a8773fdcc5a770ba
albertoflorence <alberto.sabatier@gmail.com>
2024-02-27 17:45:52 -0300

    requisito 8

---
+++ app/src/main/java/com/betrybe/currencyview/data/api/Api.kt
@@ -0,0 +1,27 @@
+package com.betrybe.currencyview.data.api
+
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Api {
+    private var retrofit: Retrofit? = null
+    fun getInstance(): ApiService {
+        if (retrofit == null) {
+            val headerInterceptor = Interceptor { chain ->
+                val request = chain.request().newBuilder()
+                    .addHeader("apikey", "aScjnJiBw0BqPSSMbpYASy36KywNBNTV")
+                    .build()
+                chain.proceed(request)
+            }
+            val client = OkHttpClient.Builder().addInterceptor(headerInterceptor).build()
+            retrofit =
+                Retrofit.Builder().baseUrl("https://api.apilayer.com/exchangerates_data/")
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create()).build()
+        }
+
+        return retrofit?.create(ApiService::class.java)!!
+    }
+}
--- app/src/main/java/com/betrybe/currencyview/ui/views/activities/MainActivity.kt
+++ app/src/main/java/com/betrybe/currencyview/ui/views/activities/MainActivity.kt
@@ -1,13 +1,59 @@
 package com.betrybe.currencyview.ui.views.activities
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.betrybe.currencyview.R
+import com.betrybe.currencyview.common.ApiIdlingResource
+import com.betrybe.currencyview.data.api.Api
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    private val mExchangeList: RecyclerView by lazy { findViewById(R.id.currency_rates_state) }
+    private val mAutoComplete: AutoCompleteTextView by lazy { findViewById(R.id.currency_selection_input_layout) }
+    private val mSelectState: TextView by lazy { findViewById(R.id.select_currency_state) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.updateCurrencySymbols()
+    }
+
+    private fun updateCurrencySymbols() {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                ApiIdlingResource.increment()
+                withContext(Dispatchers.Main) {
+                    val api = Api.getInstance()
+                    val response = api.getSymbols()
+                    if (response.isSuccessful) {
+                        val exchangeData = response.body() ?: return@withContext
+                        val adapter = ArrayAdapter(
+                            this@MainActivity,
+                            android.R.layout.simple_dropdown_item_1line,
+                            exchangeData.symbols.keys.toList()
+                        )
+                        mAutoComplete.setAdapter(adapter)
+                        mSelectState.visibility = View.VISIBLE
+                    }
+                }
+                ApiIdlingResource.decrement()
+            } catch (ex: Exception) {
+                ApiIdlingResource.decrement()
+                Log.e("Error", ex.toString())
+            }
+        }
     }
 }
